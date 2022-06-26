055ef961ccb8bb2797d81ee24973e6e47e8a89b5
Vivek Jadyar <100848524+VivekJadyar@users.noreply.github.com>
2022-06-26 23:57:22 +0530

    Add files via upload

---
+++ donkey.js
@@ -0,0 +1,43 @@
+
+const mysql = require('mysql2'); //fate
+const con = mysql.createConnection(dbparams);//fate
+const express = require('express');
+const app = express();
+
+let dbparams =
+{
+    host: 'localhost',
+    user: 'root',
+    password: 'cdac',
+    database: 'pleasework',
+    port: 3306
+};
+
+
+
+app.use(express.static("cp"));
+
+app.get("/getbal", (req, resp) => {
+
+    let input = req.query.x;
+    // console.log(input);
+    let output = { bankfoundstatus: false };
+
+    con.query('select * from bank where accno =?', [input], (error, rows) => {
+        if (rows.length > 0) {
+            output.bankfoundstatus = true;
+            output.bankdetails = rows[0];
+            output.bankdetails.bal = output.bankdetails.bal * 12;
+        }
+        resp.send(output);
+    }
+
+    
+    );
+
+});
+
+
+app.listen(900, function () {
+    console.log("server listening at port 900...");
+});
